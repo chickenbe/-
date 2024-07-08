94c5b408ef4ad8ede9b3b91c0d6dcd922bf5daed
chickenbe <114602455+chickenbe@users.noreply.github.com>
2024-07-08 14:08:07 +0800

    Delete backEnd directory

--- backEnd/Springbbot/src/main/java/com/questionnaire/springbbot/data/QuestionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.questionnaire.springbbot.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionsRepository extends JpaRepository<Questions,Long> {
-
-}
--- backEnd/Springbbot/src/main/java/com/questionnaire/springbbot/service/ChoicesService.java
+++
@@ -1,9 +0,0 @@
-package com.questionnaire.springbbot.service;
-
-import com.questionnaire.springbbot.data.Choices;
-
-import java.util.List;
-
-public interface ChoicesService {
-    public List<Choices> getAllChoices(long creator_id, long test_id,long question_id);
-}
